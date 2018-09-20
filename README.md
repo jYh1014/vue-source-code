b1874215a6826f87c4762e5a8c1f368feb8068f8
likui <likui@ttk.com>
2018-09-20 20:12:51 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-"# vue-source-code" 
+# vue-for-learning
+
+Learning vue.js source code progressively and effectively
+
+### what you will get
+
+Totally understand how does vue actually work with minimum cost
+
+### stage-1
+
+- use virtual dom to represent real dom 
+- mount virtual dom to real dom
+
+> learn `vnode`, `createElm`
+
+### stage-2
+
+- add proxy and vue constructor
+- and prepare for next stage
+
+> learn `proxy`
--- vue-02/vue-0.2.js
+++ vue-02/vue-0.2.js
@@ -2,9 +2,9 @@
 //step1:
 //参数options:{el:"#demo",data:{message:"hello"},render:function createElement(){}}
 
-(function(){
+(function () {
   //创建虚拟dom
-  function vnode (tag, data, children, text, elm) {
+  function vnode(tag, data, children, text, elm) {
     this.tag = tag;
     this.data = data;
     this.children = children;
@@ -12,131 +12,130 @@
     this.elm = elm;
   }
 
-//创建节点
-function createElm(vnode){
-  let data = vnode.data
-  let tag = vnode.tag
-  let children = vnode.children
-  if(tag){
-    vnode.elm = document.createElement(tag) 
-    if(data){
-      let attrs = data
-     
-      traversalObject(attrs, vnode.elm)
+  //创建节点
+  function createElm(vnode) {
+    console.log(vnode)
+    let data = vnode.data
+    let tag = vnode.tag
+    let children = vnode.children
+    if (tag) {
+      vnode.elm = document.createElement(tag)
+      if (data) {
+        let attrs = data
+        traversalObject(attrs, vnode.elm)
+      }
+      if (children) {
+        createChildren(vnode, children)
+      }
+    } else {
+      vnode.elm = document.createTextNode(vnode.text)
     }
-    if(children){
-      createChildren(vnode, children)
+    return vnode.elm
+  }
+  function createElement(tag, data, children, text) {
+    return new vnode(tag, data, normalizeChildren(children), createTextVNode(text))
+  }
+
+  function normalizeChildren(children) {
+    if (typeof children == 'string') {
+      return [createTextVNode(children)]
     }
-  }else{
-    vnode.elm = document.createTextNode(vnode.text)
+    return children
   }
-  return vnode.elm
-}
-function createElement(tag, data, children, text){
-  return new vnode(tag, data, normalizeChildren(children), createTextVNode(text))
-}
 
-function normalizeChildren(children){
-  if(typeof children == 'string'){
-    return [createTextVNode(children)]
+  function createTextVNode(text) {
+    return new vnode(undefined, undefined, undefined, String(text))
   }
-  return children
-}
 
-function createTextVNode(text){
-  return new vnode(undefined, undefined, undefined, String(text)) 
-}
+  function traversalObject(obj, elm) {//深层遍历对象
+    for (let key in obj) {
+      if (typeof obj[key] == 'object') {
+        traversalObject(obj[key], elm)
+      } else {
+        elm.setAttribute(key, obj[key])
+      }
 
-function traversalObject(obj, elm){//深层遍历对象
-  for(let key in obj){
-    if(typeof obj[key] == 'object'){
-      traversalObject(obj[key], elm)
-    }else{
-     elm.setAttribute(key,obj[key])
     }
-    
   }
-}
-function createChildren(vnode, children){
-  for(let i = 0;i < children.length; i ++){
-    vnode.elm.appendChild(createElm(children[i]))
+  function createChildren(vnode, children) {
+    for (let i = 0; i < children.length; i++) {
+      vnode.elm.appendChild(createElm(children[i]))
+    }
   }
-}
-function Vue(options){
-  debugger
-  this.$options = options
-  initData(this)
-  this.mount(document.querySelector(options.el))
-}
-Vue.prototype.mount = function(el){
-  this.$el = el
-  let vnode = this.$options.render.call(this)
-  this.patch(this.$el, vnode)
-}
-Vue.prototype.patch = function(oldVnode, vnode){
-    
+  function Vue(options) {
+    debugger
+    this.$options = options
+    initData(this)
+    this.mount(document.querySelector(options.el))
+  }
+  Vue.prototype.mount = function (el) {
+    this.$el = el
+    let vnode = this.$options.render.call(this)
+    this.patch(this.$el, vnode)
+  }
+  Vue.prototype.patch = function (oldVnode, vnode) {
+
     let isRealElement = oldVnode.nodeType !== undefined
-    if(isRealElement){
+    if (isRealElement) {
       createElm(vnode)
       var parent = oldVnode.parentNode
-      if(parent){
+      if (parent) {
         parent.insertBefore(vnode.elm, oldVnode)
         parent.removeChild(oldVnode)
       }
     }
-  
-
-  return vnode.elm
-}
-
-function initData(vm){
-  
-  let data = vm.$data = vm.$options.data
-  let keys = Object.keys(data)
-  let i = keys.length
-  while(i --){
-    proxy(vm, keys[i])
+    return vnode.elm
   }
-}
-function proxy(vm, key){
-  Object.defineProperty(vm, key, {
-    configurable: true,
-    enumerable: true,
-    get: function(){
-      return vm.$data[key]
+
+  function initData(vm) {
+
+    let data = vm.$data = vm.$options.data
+    let keys = Object.keys(data)
+    let i = keys.length
+    while (i--) {
+      proxy(vm, keys[i])
+    }
+  }
+  function proxy(vm, key) {
+    Object.defineProperty(vm, key, {
+      configurable: true,
+      enumerable: true,
+      get: function () {
+        return vm.$data[key]
+      },
+      set: function (val) {
+        vm.$data[key] = val
+      }
+    })
+  }
+  let vue = new Vue({
+    el: '#app',
+    data: {
+      message: 'hello'
     },
-    set: function(val){
-      vm.$data[key] = val
+    render: function () {
+      return createElement(
+        'div',
+        {
+          'class': 'outer',
+
+          attrs: {
+            id: 'content'
+          },
+
+        },
+        [
+          createElement(
+            'h2',
+            {
+              'class': 'inner'
+            },
+            this.message
+          )
+        ]
+      )
     }
   })
-}
-let vue = new Vue({
-  el: '#app',
-  data: {
-    message: 'hello'
-  },
-  render: function(){
-    return createElement(
-      'div',
-      {
-        'class': 'outer',
-        
-        attrs: {
-          id: 'content'
-        }
-      },
-      [
-        createElement(
-          'h2',
-          { 
-            'class': 'inner'
-          },
-          this.message
-        )
-      ]
-    )
-  }
-})
 
 })()
 
